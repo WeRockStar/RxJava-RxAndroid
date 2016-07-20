88dae46a16c4acbaeaffde476924613d08de168d
WeRockStar <vr_freestyle@outlook.com>
2016-07-21 00:30:48 +0700

    Added more samble

--- app/src/main/java/com/werocksta/rxjavaplayground/presenter/BasicPresenter.java
+++ app/src/main/java/com/werocksta/rxjavaplayground/presenter/BasicPresenter.java
@@ -3,6 +3,9 @@ package com.werocksta.rxjavaplayground.presenter;
 import android.util.Log;
 
 import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.Subscription;
 
 public class BasicPresenter {
 
@@ -26,5 +29,47 @@ public class BasicPresenter {
 
     private int displayLogs() {
         return Log.d("Completed", "Completed");
+    }
+
+    /**
+     * Subscription
+     */
+    public void inputText2(final String text) {
+        Subscription s = Observable.just(text).subscribe(
+                view::onTextChange,
+                Throwable::printStackTrace,
+                this::displayLogs
+        );
+
+
+        // unsubscribe avoid memory leak
+        s.unsubscribe();
+    }
+
+    /**
+     * Subscriber
+     */
+    public void inputText3(final String text) {
+        Observable.just(text)
+                .subscribe(
+                        new Subscriber<String>() {
+                            @Override
+                            public void onCompleted() {
+
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+
+                            }
+
+                            @Override
+                            public void onNext(String s) {
+                                // unsubscribe avoid memory leak
+                                if (s.equals("unsubscribe"))
+                                    unsubscribe();
+                            }
+                        }
+                );
     }
 }
