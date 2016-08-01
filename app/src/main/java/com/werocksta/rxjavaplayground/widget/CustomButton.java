5eab352abd4afecd109e808642930d5a4098f53d
WeRockStar <vr_freestyle@outlook.com>
2016-08-01 22:06:45 +0700

    Prepare custom view

--- app/src/main/java/com/werocksta/rxjavaplayground/widget/CustomButton.java
+++ app/src/main/java/com/werocksta/rxjavaplayground/widget/CustomButton.java
@@ -1,4 +1,26 @@
 package com.werocksta.rxjavaplayground.widget;
 
-public class CustomButton {
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class CustomButton extends Button {
+    public CustomButton(Context context) {
+        super(context);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 }
--- app/src/main/java/com/werocksta/rxjavaplayground/widget/CustomTextView.java
+++ app/src/main/java/com/werocksta/rxjavaplayground/widget/CustomTextView.java
@@ -1,5 +1,28 @@
 package com.werocksta.rxjavaplayground.widget;
 
 
-public class CustomTextView {
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class CustomTextView extends TextView {
+
+    public CustomTextView(Context context) {
+        super(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 }
